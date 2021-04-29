530956c7095c95119f32acc66ddad7c890bdf0dd
giu0213@naver.com <giu0213@naver.com>
2021-04-29 16:46:12 +0900

    GridCard 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Section/MainImage';
+import GridCards from '../Commons/GridCards';
+import {Row} from 'antd';
 
 function LandingPage() {
 
@@ -14,8 +16,8 @@ function LandingPage() {
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
-                setMovies([response.results])
-                console.log(response)
+                console.log(response.results)
+                setMovies(...[response.results])
                 setMainMovieImage(response.results[0])
             });
 
@@ -25,8 +27,8 @@ function LandingPage() {
         <div style={{ width: '100%', margin: '0' }}>
 
             {MainMovieImage &&
-                <MainImage 
-                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
                     title={MainMovieImage.original_title}
                     overview={MainMovieImage.overview}
                 />
@@ -36,7 +38,17 @@ function LandingPage() {
                 <h2>Movies by latest</h2>
                 <hr />
 
-                {/*Movie Grid Cards*/}
+                <Row gutter={[16,16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={movie.poster_path ? `${IMAGE_BASE_URL}w300${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
 
             </div>
 
