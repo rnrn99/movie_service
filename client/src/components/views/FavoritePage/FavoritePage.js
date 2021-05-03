56b5eb78f1f6afe8a8e93392ac67a10dc97ed461
giu0213@naver.com <giu0213@naver.com>
2021-05-03 20:02:25 +0900

    pop over 처리

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './FavoritePage.css';
+import { IMAGE_BASE_URL} from '../../Config';
 import axios from 'axios';
+import { Popover } from 'antd';
 
 function FavoritePage() {
 
@@ -17,6 +19,27 @@ function FavoritePage() {
             })
     }, [])
 
+    const renderCards = Favorites.map((favorite, index) => {
+
+        const content = (
+            <div>
+                {favorite.movieImage ? 
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.movieImage}`}/> : "no image"
+                }
+            </div>
+        )
+
+         return <tr key={index}>
+            <Popover content={content} title={`${favorite.movieTitle}`}>
+                <td>{favorite.movieTitle}</td>
+            </Popover>
+            <td>{favorite.movieRunTime} mins</td>
+            <td>
+                <button>Remove</button>
+            </td>
+        </tr>
+    });
+
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h2>Favorite Movies</h2>
@@ -30,15 +53,7 @@ function FavoritePage() {
                     </tr>
                 </thead>
                 <tbody>
-                    {Favorites.map((favorite, index) => (
-                        <tr key={index}>
-                            <td>{favorite.movieTitle}</td>
-                            <td>{favorite.movieRunTime} mins</td>
-                            <td>
-                                <button>Remove</button>
-                            </td>
-                        </tr>
-                    ))}
+                    { renderCards }
                 </tbody>
             </table>
         </div>
